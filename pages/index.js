f0689094c34a19ac87f417a486d540d38c16f439
shota <tontonka2@gmail.com>
2022-03-07 10:19:06 +0900

    add ResoucePicker

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,34 @@
 import React, { useState } from 'react';
 import { Page, Layout, EmptyState } from "@shopify/polaris";
+import { ResourcePicker, TitleBar } from '@shopify/app-bridge-react';
 
 const img = "https://cdn.shopify.com/s/files/1/0757/9955/files/empty-state.svg";
 
 const Index = () => {
-  // 後ほど使用する
   const [open, setOpen] = useState(false)
+
+  const handleSelection = (resources) => {
+    setOpen(false)
+    console.log(resources);
+  }
 
   return (
     <Page>
+      <TitleBar
+        primaryAction={{
+          content: 'Select products',
+          onAction: () => setOpen(true),
+        }}
+      />
+
+      <ResourcePicker // Resource picker component
+        resourceType="Product"
+        showVariants={false}
+        open={open}
+        onSelection={(resources) => handleSelection(resources)}
+        onCancel={() => setOpen(false)}
+      />
+
       <Layout>
         <EmptyState // Empty state component
           heading="Discount your products temporarily"
