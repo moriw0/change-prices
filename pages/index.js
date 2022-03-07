4a345b22c52c2a38cc0f58cd49085bc65f4cb729
shota <tontonka2@gmail.com>
2022-03-07 10:56:32 +0900

    change to syntax

--- pages/index.js
+++ pages/index.js
@@ -1,28 +1,48 @@
-import React from 'react';
-import { Heading, Page, TextStyle, Layout, EmptyState} from "@shopify/polaris";
+import React, { useState } from 'react';
+import { Page, Layout, EmptyState } from "@shopify/polaris";
+import { ResourcePicker, TitleBar } from '@shopify/app-bridge-react';
 
-const img = 'https://cdn.shopify.com/s/files/1/0757/9955/files/empty-state.svg';
+const img = "https://cdn.shopify.com/s/files/1/0757/9955/files/empty-state.svg";
 
-class Index extends React.Component {
-  state = { open: false };
-  render() {
-    return (
-      <Page>
-        <Layout>
-          <EmptyState // Empty state component
-            heading="Discount your products temporarily"
-            action={{
-              content: 'Select products',
-              onAction: () => this.setState({ open: true }),
-            }}
-            image={img}
-          >
-            <p>Select products to change their price temporarily.</p>
-          </EmptyState>
-        </Layout>
-      </Page>
-    )
+const Index = () => {
+  const [open, setOpen] = useState(false)
+
+  const handleSelection = (resources) => {
+    setOpen(false)
+    console.log(resources);
   }
-};
+
+  return (
+    <Page>
+      <TitleBar
+        primaryAction={{
+          content: 'Select products',
+          onAction: () => setOpen(true),
+        }}
+      />
+
+      <ResourcePicker // Resource picker component
+        resourceType="Product"
+        showVariants={false}
+        open={open}
+        onSelection={(resources) => handleSelection(resources)}
+        onCancel={() => setOpen(false)}
+      />
+
+      <Layout>
+        <EmptyState // Empty state component
+          heading="Discount your products temporarily"
+          action={{
+            content: "Select products",
+            onAction: () => setOpen(true)
+          }}
+          image={img}
+        >
+          <p>Select products to change their price temporarily.</p>
+        </EmptyState>
+      </Layout>
+    </Page>
+  )
+}
 
 export default Index;
