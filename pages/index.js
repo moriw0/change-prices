60f36173f9521ff0971fceb3a927fba252e47f2a
shota <tontonka2@gmail.com>
2022-03-07 10:26:06 +0900

    add ResourceList

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,20 @@
 import React, { useState } from 'react';
 import { Page, Layout, EmptyState } from "@shopify/polaris";
 import { ResourcePicker, TitleBar } from '@shopify/app-bridge-react';
+import ResourceListWithProducts from "./components/ResourceList";
+import store from 'store-js';
 
 const img = "https://cdn.shopify.com/s/files/1/0757/9955/files/empty-state.svg";
 
 const Index = () => {
   const [open, setOpen] = useState(false)
+  const emptyState = !store.get('ids');
 
   const handleSelection = (resources) => {
     setOpen(false)
-    console.log(resources);
+    console.log(resources)
+    const idsFromResources = resources.selection.map((product) => product.id);
+    store.set('ids', idsFromResources);
   }
 
   return (
@@ -29,18 +34,23 @@ const Index = () => {
         onCancel={() => setOpen(false)}
       />
 
-      <Layout>
-        <EmptyState // Empty state component
-          heading="Discount your products temporarily"
-          action={{
-            content: "Select products",
-            onAction: () => setOpen(true)
-          }}
-          image={img}
-        >
-          <p>Select products to change their price temporarily.</p>
-        </EmptyState>
-      </Layout>
+      {emptyState ? ( // Controls the layout of your app's empty state
+        <Layout>
+          <EmptyState
+            heading="Discount your products temporarily"
+            action={{
+              content: 'Select products',
+              onAction: () => setOpen(true),
+            }}
+            image={img}
+          >
+            <p>Select products to change their price temporarily.</p>
+          </EmptyState>
+        </Layout>
+      ) : (
+        // Uses the new resource list that retrieves products by IDs
+        <ResourceListWithProducts />
+      )}
     </Page>
   )
 }
