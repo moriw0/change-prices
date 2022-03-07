5d3986779924f408eccdaa101d862ff0ed969967
shota <tontonka2@gmail.com>
2022-03-07 09:33:01 +0900

    change to functional syntax

--- pages/index.js
+++ pages/index.js
@@ -1,28 +1,28 @@
-import React from 'react';
-import { Heading, Page, TextStyle, Layout, EmptyState} from "@shopify/polaris";
+import React, { useState } from 'react';
+import { Page, Layout, EmptyState } from "@shopify/polaris";
 
-const img = 'https://cdn.shopify.com/s/files/1/0757/9955/files/empty-state.svg';
+const img = "https://cdn.shopify.com/s/files/1/0757/9955/files/empty-state.svg";
 
-class Index extends React.Component {
-  state = { open: false };
-  render() {
-    return (
-      <Page>
-        <Layout>
-          <EmptyState // Empty state component
-            heading="Discount your products temporarily"
-            action={{
-              content: 'Select products',
-              onAction: () => this.setState({ open: true }),
-            }}
-            image={img}
-          >
-            <p>Select products to change their price temporarily.</p>
-          </EmptyState>
-        </Layout>
-      </Page>
-    )
-  }
-};
+const Index = () => {
+  // 後ほど使用する
+  const [open, setOpen] = useState(false)
+
+  return (
+    <Page>
+      <Layout>
+        <EmptyState // Empty state component
+          heading="Discount your products temporarily"
+          action={{
+            content: "Select products",
+            onAction: () => setOpen(true)
+          }}
+          image={img}
+        >
+          <p>Select products to change their price temporarily.</p>
+        </EmptyState>
+      </Layout>
+    </Page>
+  )
+}
 
 export default Index;
