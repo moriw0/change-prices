7375a6999bb0f896015c147cbcbaa9c20ada31d6
shota <tontonka2@gmail.com>
2022-03-07 15:30:52 +0900

    add randomprice

--- pages/components/ResourceList.js
+++ pages/components/ResourceList.js
@@ -1,9 +1,10 @@
 import { Query } from 'react-apollo';
 import store from "store-js";
-import React from "react";
+import React, { useState } from "react";
 import { Card, ResourceList, Stack, TextStyle, Thumbnail } from "@shopify/polaris";
 
 import gql from 'graphql-tag';
+import ApplyRandomPrices from "./ApplyRandomPrices";
 
 const GET_PRODUCTS_BY_ID = gql`
   query getProducts($ids: [ID!]!) {
@@ -35,60 +36,95 @@ const GET_PRODUCTS_BY_ID = gql`
 `;
 
 const ResourceListWithProducts = () => {
+  const [selectedItems, setSelectedItems] = useState([]);
+  const [selectedNodes, setSelectedNodes] = useState({});
+  
   return (
     <Query query={GET_PRODUCTS_BY_ID} variables={{ ids: store.get('ids') }}>
-      {({ data, loading, error }) => {
+      {({ data, loading, error, refetch }) => {
         if (loading) return <div>Loading…</div>;
         if (error) return <div>{error.message}</div>;
         
+        const nodesById = {};
+        data.nodes.forEach(node => nodesById[node.id] = node);
+
+        const handleSelectionChange = (selectedItems) => {
+          const selectedNodes = {};
+          selectedItems.forEach(item => selectedNodes[item] = nodesById[item]);
+
+          setSelectedItems(selectedItems)
+          setSelectedNodes(selectedNodes)
+        }
+
+        const handleClickItem = (itemId) => {
+          let index = selectedItems.indexOf(itemId);
+          const node = nodesById[itemId];
+          if (index === -1) {
+            selectedItems.push(itemId)
+            selectedNodes[itemId] = node;
+          } else {
+            selectedItems.splice(index, 1);
+            delete selectedNodes[itemId];
+          }
+
+          setSelectedItems(selectedItems)
+          setSelectedNodes(selectedNodes)
+        }
+
         return (
-        <Card>
-          <ResourceList // Defines your resource list component
-            showHeader
-            resourceName={{ singular: 'Product', plural: 'Products' }}
-            items={data.nodes}
-            renderItem={item => {
-              const media = (
-                <Thumbnail
-                  source={
-                    item.images.edges[0]
-                      ? item.images.edges[0].node.id
-                      : ''
-                  }
-                  alt={
-                    item.images.edges[0]
-                      ? item.images.edges[0].node.altText
-                      : ''
-                  }
-                />
-              );
-              const price = item.variants.edges[0].node.price;
-              return (
-                <ResourceList.Item
-                  id={item.id}
-                  media={media}
-                  accessibilityLabel={`View details for ${item.title}`}
-                  onClick={() => {
-                    store.set('item', item);
-                  }}
-                >
-                  <Stack>
-                    <Stack.Item fill>
-                      <h3>
-                        <TextStyle variation="strong">
-                          {item.title}
-                        </TextStyle>
-                      </h3>
-                    </Stack.Item>
-                    <Stack.Item>
-                      <p>${price}</p>
-                    </Stack.Item>
-                  </Stack>
-                </ResourceList.Item>
-              );
-            }}
-          />
-        </Card>
+          <>
+          <Card>
+            <ResourceList
+              showHeader
+              resourceName={{ singular: 'Product', plural: 'Products' }}
+              items={data.nodes}
+              selectable
+              selectedItems={selectedItems}
+              onSelectionChange={handleSelectionChange}
+              renderItem={(item, index) => {
+                const media = (
+                  <Thumbnail
+                    source={
+                      item.images.edges[0]
+                        ? item.images.edges[0].node.id
+                        : ''
+                    }
+                    alt={
+                      item.images.edges[0]
+                        ? item.images.edges[0].node.altText
+                        : ''
+                    }
+                  />
+                );
+                const price = item.variants.edges[0].node.price;
+                return (
+                  <ResourceList.Item
+                    id={item.id}
+                    key={[index, price]}
+                    media={media}
+                    accessibilityLabel={`View details for ${item.title}`}
+                    verticalAlignment="center"
+                    onClick={() => handleClickItem(item.id)}
+                  >
+                    <Stack alignment="center">
+                      <Stack.Item fill>
+                        <h3>
+                          <TextStyle variation="strong">
+                            {item.title}
+                          </TextStyle>
+                        </h3>
+                      </Stack.Item>
+                      <Stack.Item>
+                        <p>${price}</p>
+                      </Stack.Item>
+                    </Stack>
+                  </ResourceList.Item>
+                );
+              }}
+            />
+          </Card>
+          <ApplyRandomPrices selectedItems={selectedNodes} onUpdate={refetch} />
+        </>
         );
       }}
     </Query>
